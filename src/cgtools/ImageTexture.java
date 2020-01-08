23bd87e98587c54ffb2e6b0926ee10406c3ecaf6
Admin <Admin@GKBeat.Speedport_W_724V_09011603_06_003>
2020-01-08 18:10:28 +0100

    a10smolChange

--- src/cgtools/ImageTexture.java
+++ src/cgtools/ImageTexture.java
@@ -57,25 +57,6 @@ public class ImageTexture implements Sampler {
         return divide(color, componentScale);
     }
     
-//    public Color getColor(double u, double v) {
-//        int x = (int) ((u - Math.floor(u)) * width);
-//        int y = (int) ((v - Math.floor(v)) * height);
-//        double[] pixelBuffer = new double[components];
-//        image.getRaster().getPixel(x, y, pixelBuffer);
-//        Color color = red;
-//        switch (components) {
-//        case 1:
-//            color = color(pixelBuffer[0], 0, 0);
-//        case 2:
-//            color = color(pixelBuffer[0], pixelBuffer[1], 0);
-//        case 3:
-//            color = color(pixelBuffer[0], pixelBuffer[1], pixelBuffer[2]);
-//        case 4:
-//            color = color(pixelBuffer[0], pixelBuffer[1], pixelBuffer[2]);
-//        }
-//        return divide(color, componentScale);
-//    }
-    
     private double gamma (double value) {
     	return Math.pow((value/componentScale), 2.2)*componentScale;
     }
